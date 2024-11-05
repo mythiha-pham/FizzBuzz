130c6dda3a42e8f73363d853f02355276e0e5157
Thi Ha My Pham <hamypham2000@gmail.com>
2024-11-05 14:18:24 +0100

    Write a failing test

--- kata/test_fizzbuzz.py
+++ kata/test_fizzbuzz.py
@@ -1,4 +1,14 @@
-from kata.fizzbuzz import fizzbuzz
+from fizzbuzz import fizzbuzz
+
+def load_expected_outputs(filename):
+    expected_outputs = {}
+    with open(filename, 'r') as file:
+        for number in range(1, 101):
+            expected_outputs[number] = file.readline().strip()  # Read each line for numbers 1 to 100
+    return expected_outputs
 
 def test_fizzbuzz():
-    assert fizzbuzz(0) == 0
+    expected_outputs = load_expected_outputs('testdata/fizzbuzz_stage1.txt')
+    
+    for number in range(1, 101):
+        assert fizzbuzz(number) == expected_outputs[number]
